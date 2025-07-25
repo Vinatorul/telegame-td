4a2aa3e9a5f78899e466ce17c0a7f79e73d30111
Александр Куваев <vinatorul@yandex-team.ru>
2025-07-25 23:24:02 +0300

    fix playwright

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ export default {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^phaser$': '<rootDir>/node_modules/phaser/dist/phaser.js'
